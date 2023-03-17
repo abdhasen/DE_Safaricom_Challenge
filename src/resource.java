ebbb18b2a61a47b02a4de285a6430300071c36aa
abdhasen <76993301+abdhasen@users.noreply.github.com>
2023-03-17 12:46:13 +0300

    Update resource.java

--- src/resource.java
+++ src/resource.java
@@ -1,16 +1,28 @@
-public class Resource {
+import java.io.IOException;
+import java.util.*;
+public class resource {
 
     try{
+        // create FileOutPutstream object
         FileOutputStream fos = new FileOutputStream();
+        // message to be written
         String str = "Welcome to Safaricom";
+
         byte bArray[] = str.getBytes();
-        fos.write(bArray);
+        try {
+            fos.write(bArray);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
         System.out.println("message is written to a file");
+        try {
+            fos.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
     catch(Exception e)
-
     {
         System.out.println(e);
     }
-}
 }
