7f5fb199803757e1618f6e6e58aa6329d26a9bab
abdhasen <76993301+abdhasen@users.noreply.github.com>
2023-03-17 12:42:13 +0300

    Update resource.java

--- src/resource.java
+++ src/resource.java
@@ -1,2 +1,16 @@
-public class resource {
+public class Resource {
+
+    try{
+        FileOutputStream fos = new FileOutputStream();
+        String str = "Welcome to Safaricom";
+        byte bArray[] = str.getBytes();
+        fos.write(bArray);
+        System.out.println("message is written to a file");
+    }
+    catch(Exception e)
+
+    {
+        System.out.println(e);
+    }
+}
 }
