dc241cfc0848574acac5bb26c7a0c53bd17d368d
abdhasen <76993301+abdhasen@users.noreply.github.com>
2023-03-17 12:21:39 +0300

    Update StrReverse.java

--- src/StrReverse.java
+++ src/StrReverse.java
@@ -1,4 +1,9 @@
 
 public class StrReverse {
-
+    String str = "entoto";
+    char[] strChar = str.toCharArray();
+        for(int i = strChar.length - 1; i >= 0; i--)
+    {
+        System.out.print(strChar[i]);
+    }
 }
